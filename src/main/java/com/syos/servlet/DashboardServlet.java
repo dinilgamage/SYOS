1b6e5c0fc4e1d9881d599ffdce0aa72a5c11c28e
Dinil Gamage <dinilgamage5@gmail.com>
2025-01-24 01:13:12 +0530

    Dashboard cart counter

--- src/main/java/com/syos/servlet/DashboardServlet.java
+++ src/main/java/com/syos/servlet/DashboardServlet.java
@@ -1,6 +1,7 @@
 package com.syos.servlet;
 
 import com.syos.dao.impl.InventoryDaoImpl;
+import com.syos.service.CartService;
 import com.syos.service.InventoryService;
 import com.syos.model.Inventory;
 
@@ -9,21 +10,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @WebServlet("/dashboard")
 public class DashboardServlet extends HttpServlet {
   private InventoryService inventoryService;
+  private CartService cartService;
 
   @Override
   public void init() throws ServletException {
     this.inventoryService = new InventoryService(new InventoryDaoImpl());
+    this.cartService = new CartService();
   }
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+    Integer userId = (Integer) request.getSession().getAttribute("userId");
+    if (userId == null) {
+      response.sendRedirect("login.jsp");
+      return;
+    }
+
     List<Inventory> inventoryItems = inventoryService.getAllItems();
+
+    // this slows down the login time by 2 sec
+    int cartSize = cartService.getCartSize(userId);
+
     request.setAttribute("inventoryItems", inventoryItems);
+    request.setAttribute("cartSize", cartSize);
+    request.getSession().setAttribute("cartSize", cartSize);
     request.getRequestDispatcher("dashboard.jsp").forward(request, response);
   }
 }
